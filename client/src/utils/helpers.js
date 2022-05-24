4b48a852c689c8e380278981d48cdf475be7861e
a-vitug <vtg.allyson@gmail.com>
2022-05-24 16:22:37 -0700

    prettier code

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,21 @@
+// email validation
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+/* password validation 
+    Minimum eight characters, 
+    at least one uppercase letter, 
+    one lowercase letter, 
+    one number and 
+    one special character */
+
+export function checkPassword(input) {
+    const passw = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (input.match(passw)) {
+        return true;
+    }
+    return false;
+}
+  
