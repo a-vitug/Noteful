a8fe0b844bf65fdba30cf4a1ceef2fe08a5f02f1
aldreanelafont <93751029+aldreanelafont@users.noreply.github.com>
2022-05-28 21:40:53 -0700

    initial commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,12 @@ const { connect, connection } = require('mongoose');
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
   //TODO: Need to do mongo
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/usersDB';
 
+
+let uri = 'mongodb+srv://mongodb:atlaspass@socialmedia.l9xbo.mongodb.net/db1?retryWrites=true&w=majority'
+let mongoose = require('mongoose')
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
 connect(connectionString, {
   useNewUrlParser: true,
