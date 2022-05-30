a38e27b0288b4c4caaba1d251d5ca22fa215bf72
Jasper Barcial <Jbarcial25@gmail.com>
2022-05-30 02:31:52 -0700

    added mutations on adding adding user and posts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,6 +23,64 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in')
         }
+    },
+
+    Mutation: {
+        addUser: async (parent, {username, email, password}) => {
+            const user = await User.create({ username, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, { email, password}) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this address');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('No user/email found')
+            }
+
+            const token = signToken(user);
+
+            return { token, user};
+        },
+        addPost: async (parent, { thoughText }, context) => {
+            if (context.user) {
+                const post = await Post.create({
+                    postText,
+                    postAuthor: context.user.username,
+                });
+
+                await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $addToSet: { thoughts: thoughText._id}}
+                );
+
+                return post;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        removePost: async (parent, { throughId }, context) => {
+            if (context.user) {
+                const post = await Post.findOneAndDelete({
+                    _id: postId,
+                    postAuthor: context.user.username,
+                });
+
+                await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    { $pull: { posts: post._id}}
+                );
+
+                return post;
+            }
+
+            throw new AuthenticationError('You need to be logged in!')
+        }
     }
 }
 
