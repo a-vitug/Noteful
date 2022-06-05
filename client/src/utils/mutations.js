3e0807f0b5ee744984390775a6c011c908868d9a
Jasper Barcial <Jbarcial25@gmail.com>
2022-06-05 14:18:58 -0700

    Fixed mutations on client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-    mutation login(email: $String!, password: $String!) {
+    mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password){
             token
             user {
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-    mutation addUser(username: $String!, email: $String!, password: $String!) {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token
             user {
@@ -25,7 +25,7 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_POST = gql`
-    mutation addPost(postText: $String!, postAuthor: $String!) {
+    mutation addPost($postText: String!, $postAuthor: String!) {
         addPost(postText: $postText, postAuthor: $postAuthor){
             _id
             postText
@@ -40,7 +40,7 @@ export const ADD_POST = gql`
 `;
 
 export const CREATE_COMMENT = gql`
-    mutation createComment($postId: ID!, commentText: $String!, commentAuthor: $String!) {
+    mutation createComment($postId: ID!, $commentText: String!, $commentAuthor: String!) {
         createComment(postId: $postId, commentText: $commentText, commentAuthor: $commentAuthor) {
             _id
             postText
