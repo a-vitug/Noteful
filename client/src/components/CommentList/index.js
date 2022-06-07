27985dd3cbe723e0c36813870c492250f7d66b2e
Kyle Yang <omfgkyyy@gmail.com>
2022-06-07 03:30:17 -0700

    Comment list fixed

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -37,29 +37,32 @@ const CommentList = ({ comments = [] }) => {
     };
 
     return(
-      <>
-             <Box m={10}>
-                {comments &&
-                comments.map((comment) => (
-                    <FormControl isReadOnly id='comment' key={comment.id}>
-                        <FormLabel color={textcolor}> {comment.commentAuthor} </FormLabel>
-                        <InputGroup
-                            size='md'
-                            boxShadow='lg'
-                        >
-                            <Input h='65px' backgroundColor={bgcolor}
-                                variant='filled'
-                                type='comment'
-                                placeholder={comment.commentText}
-                            />
-                            
-                        </InputGroup>
-                    </FormControl>
-                ))}
-                </Box>
+        <>
+        <h3
+        className="p-5 display-inline-block"
+        style={{ borderBottom: '1px dotted #1a1a1a' }}
+      >
+
+      </h3>
+      <div className="flex-row my-4">
+        {comments &&
+          comments.map((comment) => (
+            <div key={comment._id} className="col-12 mb-3 pb-3">
+              <div className="p-3 bg-dark text-light">
+                <h5 className="card-header">
+                  {comment.commentAuthor} commented{' '}
+                  <span style={{ fontSize: '0.825rem' }}>
+                    on {comment.createdAt}
+                  </span>
+                </h5>
+                <p className="card-body">{comment.commentText}</p>
+              </div>
+            </div>
+          ))}
+      </div>
       </>
     )
-
+   
      
 };
 
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -32,14 +32,28 @@ import { useColorMode, useColorModeValue } from '@chakra-ui/color-mode';
 import { FaSun, FaMoon, FaGithub, FaUser, FaPaperPlane, FaHeart, FaTrashAlt, FaArrowCircleLeft } from 'react-icons/fa';
 
 import CommentList from '../components/CommentList'
+import { QUERY_SINGLE_POST } from '../utils/queries';
+
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
 
 
 const Post = () => {
+    const { postId } = useParams();
+
+    const { loading, data } = useQuery(QUERY_SINGLE_POST, {
+        // pass URL parameter
+        variables: { postId: postId },
+      });
+
+    const post = data?.post || {}
 
     const { colorMode, toggleColorMode } = useColorMode();
     const isDark = colorMode === 'dark';
     const textcolor = useColorModeValue('#BFAE98', '#E8DFD8');
     const bgcolor = useColorModeValue('RGBA(0, 0, 0, 0.16)', 'RGBA(0, 0, 0, 0.36)');
+
+    
 
     let [value, setValue] = React.useState('')
 
@@ -47,6 +61,9 @@ const Post = () => {
     let inputValue = e.target.value;
     setValue(inputValue);
     };
+     if (loading) {
+    return <div>Loading...</div>;
+  }
 
     return (
         <Stack className='postbg' pt={100}>
@@ -55,7 +72,7 @@ const Post = () => {
                 <IconButton icon={<FaArrowCircleLeft />} isRound='true' />
                 <Box m={3}>
                     <FormControl isReadOnly id='comment' >
-                        <FormLabel color={textcolor}> username </FormLabel>
+                        <FormLabel color={textcolor}> {post.postAuthor} </FormLabel>
                         <InputGroup
                             size='md'
                             boxShadow='lg'
@@ -63,15 +80,14 @@ const Post = () => {
                             <Input h='65px' backgroundColor={bgcolor}
                                 variant='filled'
                                 type='comment'
-                                placeholder='this is my very first post yay ~~~ '
+                                placeholder = {post.postText}
                             />
-                            
                         </InputGroup>
                     </FormControl>
                 </Box>
 
                 <Divider></Divider>
-                <CommentList> /</CommentList>
+                {/* <CommentList> /</CommentList> */}
                 {/* user #2's comment */}
                 <></>
                 {/* <Box m={10}>
@@ -94,17 +110,16 @@ const Post = () => {
                 {/* user #3's comment */}
                 <Box m={10}>
                     <FormControl isReadOnly id='comment' >
-                        <FormLabel color={textcolor}> username2 </FormLabel>
+                        <FormLabel color={textcolor}> {post.postAuthor}  </FormLabel>
                         <InputGroup
                             size='md'
                             boxShadow='lg'
                         >
-                            <Input h='65px' backgroundColor={bgcolor}
+                            <CommentList h='65px' backgroundColor={bgcolor}
                                 variant='filled'
                                 type='comment'
-                                placeholder='this is my very first post yay ~~~ '
+                                comments = {post.comments}
                             />
-                            
                         </InputGroup>
                     </FormControl>
                 </Box>
@@ -112,15 +127,15 @@ const Post = () => {
                 {/* user #4's comment */}
                 <Box m={10}>
                     <FormControl isReadOnly id='comment' >
-                        <FormLabel color={textcolor}> username2 </FormLabel>
+                        <FormLabel color={textcolor}> {post.postAuthor} </FormLabel>
                         <InputGroup
                             size='md'
                             boxShadow='lg'
                         >
-                            <Input h='65px' backgroundColor={bgcolor}
+                            <CommentList h='65px' backgroundColor={bgcolor}
                                 variant='filled'
                                 type='comment'
-                                placeholder='this is my very first post yay ~~~ '
+                                comments={post.comments}
                             />
                             
                         </InputGroup>
