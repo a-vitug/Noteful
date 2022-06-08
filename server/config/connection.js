ebdb8ae314f0ee4c14eaa7ddd77f290a32059f36
Jasper Barcial <Jbarcial25@gmail.com>
2022-06-07 19:17:49 -0700

    Added delete on post list

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 // 'mongodb+srv://noteful-app:notefulclone123@noteful-app.fthcj.mongodb.net/test'
-mongoose.connect( 
-    process.env.MONGO_URI || 'mongodb+srv://noteful-app:notefulclone123@noteful-app.fthcj.mongodb.net/test',
+mongoose.connect( 'mongodb://localhost/noteFullDB',
+    process.env.MONGO_URI || 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }
 );
-
+// 'mongodb+srv://noteful-app:notefulclone123@noteful-app.fthcj.mongodb.net/test'
 
 module.exports = mongoose.connection;
